a0facac4d06e1578544b4e5cfbe7d9998ed7a911
CajunAzn007 <kevinw326@gmail.com>
2019-05-19 12:13:57 -0600

    added more shape options

---
+++ js/templates.js
@@ -0,0 +1,24 @@
+var templates = ["person", "backpack", "umbrella", "handbag", "tie",
+                 "suitcase", "bicycle", "car", "motorcycle", "airplane",
+                 "bus", "train", "truck", "boat", "traffic light",
+                 "fire hydrant", "stop sign", "parking meter", "bench",
+                 "bird", "cat", "dog", "horse", "sheep", "cow", "elephant",
+                 "bear", "zebra", "giraffe", "frisbee", "skis", "snowboard",
+                 "sports ball", "kite", "baseball bat", "baseball glove",
+                 "skateboard", "surfboard", "tennis racket", "bottle",
+                 "wine glass", "cup", "fork", "knife", "spoon", "bowl",
+                 "banana", "apple", "sandwich", "orange", "broccoli", "carrot",
+                 "hot dog", "pizza", "donut", "cake", "chair", "couch",
+                 "potted plant", "bed", "dining table", "toilet", "tv",
+                 "laptop", "mouse", "remote", "keyboard", "cell phone",
+                 "microwave", "oven", "toaster", "sink", "refrigerator",
+                 "book", "clock", "vase", "scissors", "teddy bear",
+                 "hair drier", "toothbrush"]
+
+function initMenu(){
+    for(var i = 0; i < templates.length; i++){
+        var obj = document.createElement("a");
+        obj.innerHTML = templates[i].charAt(0).toUpperCase() + templates[i].slice(1);
+        $("#vertical-menu").append(obj);
+    }
+};
